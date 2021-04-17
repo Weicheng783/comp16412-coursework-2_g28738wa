bd39bb559634a018aec178490ff6c79d556d1127
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2021-04-17 13:35:58 +0100

    Added the RouteFinder basic route find.

--- src/MazeDriver.java
+++ src/MazeDriver.java
@@ -3,7 +3,7 @@ import maze.routing.*;
 
 public class MazeDriver  {
     public static void main(String args[]) throws InvalidMazeException {
-        Maze test = Maze.fromTxt("/home/csimage/GitRepos/comp16412-coursework-2_g28738wa/resources/mazes/maze1.txt");
+        Maze test = Maze.fromTxt("/home/csimage/GitRepos/comp16412-coursework-2_g28738wa/resources/mazes/maze2.txt");
         System.out.println(test.toString());
         // test.setMaze();
         RouteFinder a = new RouteFinder(test);
--- src/maze/routing/RouteFinder.java
+++ src/maze/routing/RouteFinder.java
@@ -7,18 +7,132 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import maze.*;
 
-public class RouteFinder {
+public class RouteFinder{
+    // <E> 
     private Maze maze;
-    private Stack<Tile> route;
-    private boolean finished;
+    private Stack<Tile> route = new Stack<> ();
+    private boolean finished = false;
 
-    public RouteFinder (Maze mazee){
+    private List<Tile> blackList= new LinkedList<> ();
+
+    public RouteFinder (Maze mazee) throws NoRouteFoundException {
         maze = mazee;
-        System.out.println("Successfully get maze!");
-        Tile testcase = maze.getEntrance();
-        System.out.println(testcase.toString());
-        testcase = maze.getAdjacentTile(testcase,Maze.Direction.EAST);
-        System.out.println(testcase.toString());
+        // System.out.println("Successfully get maze!");
+        // System.out.println(route.empty());
+        // Tile testcase = maze.getEntrance();
+        // System.out.println(testcase.toString());
+        // route.push(testcase);
+        // testcase = maze.getAdjacentTile(testcase,Maze.Direction.EAST);
+        // route.push(testcase);
+        // System.out.println(route.empty());
+        // System.out.println(route.search(testcase));
+        // System.out.println(testcase.toString());
+
+        // Clock-wise finding...
+        Tile entrance = maze.getEntrance();
+        Tile temp = entrance;
+        Tile next;
+        route.push(entrance);
+        while (isFinished() == false){
+            next = maze.getAdjacentTile(temp,Maze.Direction.NORTH);
+            if(next != null){
+                if(next.toString() == "x"){
+                    route.push(next);
+                    finished = true;
+                    temp = next;
+                    break;
+                }else if(next.toString() == "."){
+                    if(blackList.contains(next) || route.search(next) != -1 ){
+                        // continue;
+                    }else{
+                        route.push(next);
+                        temp = next;
+                        continue;
+                    }
+
+                }
+            }
+
+
+            next = maze.getAdjacentTile(temp,Maze.Direction.EAST);
+            if(next != null){
+                // System.out.println("east");
+                if(next.toString() == "x"){
+                    route.push(next);
+                    finished = true;
+                    temp = next;
+                    break;
+                }else if(next.toString() == "."){
+                    if(blackList.contains(next) || route.search(next) != -1 ){
+                        // continue;
+                    }else{
+                        route.push(next);
+                        temp = next;
+                        continue;
+                    }
+
+                }
+            }
+
+    
+            next = maze.getAdjacentTile(temp,Maze.Direction.SOUTH);
+            if(next != null){
+                // System.out.println("south");
+                if(next.toString() == "x"){
+                    route.push(next);
+                    finished = true;
+                    temp = next;
+                    break;
+                }else if(next.toString() == "."){
+                    if(blackList.contains(next) || route.search(next) != -1 ){
+                        // continue;
+                    }else{
+                        route.push(next);
+                        temp = next;
+                        // System.out.println("GO HERE!");
+                        continue;
+                    }
+
+                }
+            }
+
+        
+            next = maze.getAdjacentTile(temp,Maze.Direction.WEST);
+            if(next != null){
+                if(next.toString() == "x"){
+                    route.push(next);
+                    finished = true;
+                    temp = next;
+                    break;
+                }else if(next.toString() == "."){
+                    if(blackList.contains(next) || route.search(next) != -1 ){
+                        // continue;
+                    }else{
+                        route.push(next);
+                        temp = next;
+                        continue;
+                    }
+
+                }
+            }
+
+
+
+            // the very buttom option...
+            if (route.size()==1){
+                throw new NoRouteFoundException("No Route Found, please check your maze again.");
+            }else{
+                blackList.add(route.pop());
+                temp = route.peek();
+            }
+
+
+        }
+
+            System.out.println(route.size());
+            System.out.println(route);
+            System.out.println(blackList);        
+        
 
     }
 
